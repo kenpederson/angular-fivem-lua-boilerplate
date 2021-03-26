cc9d4b33eaceb4ccbf32b258f49f8a30347c60aa
Kenneth Pederson <pederson.it@gmail.com>
2021-03-26 03:43:48 -0700

    Add basic FiveM resource lua files

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fxversion "cerulean"
+game "gta5"
+
+client_script {
+  "client/*.lua",
+}
+
+server_script {
+  "server/*.lua"
+}
+
+ui_page "static/index.html"
+
+files {
+  "static/index.html",
+  "static/main.js"
+}
